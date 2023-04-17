09a18ff48a29f1f80896e602cf388783d3fc75c0
7ours <mohamedmohsencs22@gmail.com>
2023-04-17 08:45:07 +0200

    leadeeeeeeeeeeer

--- files/javascript/addStudent.js
+++ files/javascript/addStudent.js
@@ -1,31 +1,31 @@
-let button = document.querySelector(`.butt`);
 const obj = {
-    name: "a",
-    Gen: "c",
-    cp: "d",
-    ID: "cx",
-    level: "cv",
-    sta: "",
-    date: "vc",
-    dep: "c",
-    email: "c",
-    phone: "cv"
-}
-let arr2 = JSON.stringify(obj);
-window.localStorage.setItem("studentData", arr2);
-button.onclick = function() {
-    let inputs = document.querySelectorAll(`input`);
-    inputs.forEach((ele) => {
-        obj[ele.dataset.set] = ele.value;
-        ele.value = "";
-    })
-    getEleFromLocalStorage(obj);
+    Name: "",
+    Gender: "",
+    GPA: "",
+    ID: "",
+    level: "",
+    status: "",
+    date: "",
+    department: "",
+    email: "",
+    phone: ""
 }
 
-function getEleFromLocalStorage(obj) {
-    let b = window.localStorage.getItem("studentData");
-    let arr = Array.from(JSON.parse(b));
-    arr.push(obj);
-    let c = JSON.stringify(arr);
-    window.localStorage.setItem("studentData", c);
+function store()
+{
+    obj['Name'] = document.getElementById('name').value;
+    obj['Gender'] = document.getElementById('Gen').value;
+    obj['GPA'] = document.getElementById('GPA').value;
+    obj['ID'] = document.getElementById('ID').value;
+    obj['level'] = document.getElementById('level').value;
+    obj['status'] = document.getElementById('sta').value;
+    obj['date'] = document.getElementById('date').value;
+    obj['department'] = document.getElementById('dep').value;
+    obj['email'] = document.getElementById('email').value;
+    obj['phone'] = document.getElementById('phone').value;
+    let key = obj['Name'];
+    let object = JSON.stringify(obj);
+    localStorage.setItem(key,object);
 }
+
+
