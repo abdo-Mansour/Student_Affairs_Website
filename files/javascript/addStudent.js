ddaae97bc3c47f704bcee1b3cd43dc6d85c9ea65
HazemElbehary <99183215+HazemElbehary@users.noreply.github.com>
2023-04-17 01:04:10 -0700

    Update addStudent.js

--- files/javascript/addStudent.js
+++ files/javascript/addStudent.js
@@ -11,21 +11,21 @@ const obj = {
     phone: ""
 }
 
-function store()
-{
-    obj['Name'] = document.getElementById('name').value;
-    obj['Gender'] = document.getElementById('Gen').value;
-    obj['GPA'] = document.getElementById('GPA').value;
-    obj['ID'] = document.getElementById('ID').value;
-    obj['level'] = document.getElementById('level').value;
-    obj['status'] = document.getElementById('sta').value;
-    obj['date'] = document.getElementById('date').value;
-    obj['department'] = document.getElementById('dep').value;
-    obj['email'] = document.getElementById('email').value;
-    obj['phone'] = document.getElementById('phone').value;
-    let key = obj['Name'];
-    let object = JSON.stringify(obj);
-    localStorage.setItem(key,object);
+function store() {
+    let form = document.querySelector(`form`)
+    if (form.checkValidity()) {
+        obj['Name'] = document.getElementById('name').value;
+        obj['Gender'] = document.getElementById('Gen').value;
+        obj['GPA'] = document.getElementById('GPA').value;
+        obj['ID'] = document.getElementById('ID').value;
+        obj['level'] = document.getElementById('level').value;
+        obj['status'] = document.getElementById('sta').value;
+        obj['date'] = document.getElementById('date').value;
+        obj['department'] = document.getElementById('dep').value;
+        obj['email'] = document.getElementById('email').value;
+        obj['phone'] = document.getElementById('phone').value;
+        let key = obj['Name'];
+        let object = JSON.stringify(obj);
+        localStorage.setItem(key, object);
+    }
 }
-
-
