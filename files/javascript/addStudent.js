90b93aa8f70c09afafa1a11c50dab81850751fb8
HazemElbehary <99183215+HazemElbehary@users.noreply.github.com>
2023-04-17 03:58:14 -0700

    Update addStudent.js

--- files/javascript/addStudent.js
+++ files/javascript/addStudent.js
@@ -1,3 +1,69 @@
+// let button = document.querySelector(`.butt`);
+// const obj = {
+//     name: "",
+//     Gen: "",
+//     cp: "",
+//     ID: "",
+//     level: "",
+//     sta: "",
+//     date: "",
+//     dep: "",
+//     email: "",
+//     phone: ""
+// }
+// let arr2 = JSON.stringify(obj);
+// let form = document.querySelector(`form`)
+// window.localStorage.setItem("studentData", arr2);
+// button.onclick = function() {
+//     let inputs = document.querySelectorAll(`.input`);
+//     if (form.checkValidity()) {
+//         console.log(inputs)
+//         inputs.forEach((ele) => {
+//             obj[ele.dataset.set] = ele.value;
+//             ele.value = "";
+//         });
+//         getEleFromLocalStorage(obj);
+//     } else {
+//         let as = "";
+//         inputs.forEach((ele) => {
+//             as += `Please Enter ${ele.dataset.set}` + "\n";
+//         });
+//         alert(as);
+//     }
+// }
+
+// function getEleFromLocalStorage(obj) {
+//     let b = window.localStorage.getItem("studentData");
+//     let arr = Array.from(JSON.parse(b));
+//     arr.push(obj);
+//     let c = JSON.stringify(arr);
+//     window.localStorage.setItem("studentData", c);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const obj = {
     Name: "",
     Gender: "",
@@ -12,21 +78,6 @@ const obj = {
 }
 
 function store() {
-<<<<<<< HEAD
-    obj['Name'] = document.getElementById('name').value;
-    obj['Gender'] = document.getElementById('Gen').value;
-    obj['GPA'] = document.getElementById('GPA').value;
-    obj['ID'] = document.getElementById('ID').value;
-    obj['level'] = document.getElementById('level').value;
-    obj['status'] = document.getElementById('sta').value;
-    obj['date'] = document.getElementById('date').value;
-    obj['department'] = document.getElementById('dep').value;
-    obj['email'] = document.getElementById('email').value;
-    obj['phone'] = document.getElementById('phone').value;
-    let key = obj['Name'];
-    let object = JSON.stringify(obj);
-    localStorage.setItem(key, object);
-=======
     let form = document.querySelector(`form`)
     if (form.checkValidity()) {
         obj['Name'] = document.getElementById('name').value;
@@ -43,5 +94,4 @@ function store() {
         let object = JSON.stringify(obj);
         localStorage.setItem(key, object);
     }
->>>>>>> ddaae97bc3c47f704bcee1b3cd43dc6d85c9ea65
 }
