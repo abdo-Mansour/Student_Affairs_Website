cfd7b4c4160858067289936381f9479b616bdb2b
Mansour266 <mansour24@Protonmail.com>
2023-04-17 23:28:25 +0200

    Everything working except editing student data

--- files/javascript/edit_student.js
+++ files/javascript/edit_student.js
@@ -1,21 +1,52 @@
+let namee = window.localStorage.getItem("Selected_Student");
+let Genders = ['Male', 'Female'];
+let statutes = ['Active', 'Inactive'];
+document.getElementById('name').value = JSON.parse(namee).Name;
+for(let i = 0; i < 2; i++){
+    if (JSON.parse(namee).Gender === Genders[i].value)
+        document.getElementById('Gen').value = Genders[i];
+}
 
-let student_obj = Selected_Student;
 
+document.getElementById("GPA").value = JSON.parse(namee).GPA;
+document.getElementById("ID").value = parseFloat(JSON.parse(namee).date);
+for(let i = 0; i < 2; i++){
+    if (JSON.parse(namee).Gender === Genders[i].value)
+        document.getElementById('sta').value = statutes[i];
+}
+//document.getElementById("sta").value = JSON.parse(namee).status;
+document.getElementById("date").value = JSON.parse(namee).date;
+document.getElementById("dep").value = JSON.parse(namee).department;
+document.getElementById("email").value = JSON.parse(namee).email;
+document.getElementById("phone").value = JSON.parse(namee).phone;
 
-//This should clear the previous element from the memory.
-window.localStorage.removeItem(Selected_Student);
-
-document.getElementById('name').value = student_obj["Name"];
-document.getElementById("Gen").value = student_obj["Gen"];
-document.getElementById("GPA").value = student_obj["GPA"];
-document.getElementById("ID").value = student_obj["ID"];
-document.getElementById("sta").value = student_obj["level"];
-document.getElementById("date").value = student_obj["Name"];
-document.getElementById("dep").value = student_obj["department"];
-document.getElementById("email").value = student_obj["email"];
-document.getElementById("phone").value = student_obj["phone"];
 
 //This Script redirects to search after viewing student
 document.getElementById("edit_button").onclick = function () {
-        location.href = "search_student.html";
-    };
+    location.href = "search_student.html";  
+};
+//This should clear the previous element from the memory.
+//window.localStorage.removeItem(Selected_Student);
+
+//Edit student is not working
+let editButton = document.getElementById('edit_button');
+submitBtn.addEventListener('click', store);
+function store() {
+    console.log("the student is added")
+    let form = document.querySelector(`form`)
+    if (form.checkValidity()) {
+        obj['Name'] = document.getElementById('name').value;
+        obj['Gender'] = document.getElementById('Gen').value;
+        obj['GPA'] = document.getElementById('GPA').value;
+        obj['ID'] = document.getElementById('ID').value;
+        obj['level'] = document.getElementById('level').value;
+        obj['status'] = document.getElementById('sta').value;
+        obj['date'] = document.getElementById('date').value;
+        obj['department'] = document.getElementById('dep').value;
+        obj['email'] = document.getElementById('email').value;
+        obj['phone'] = document.getElementById('phone').value;
+        let key = obj['Name'];
+        let object = JSON.stringify(obj);
+        localStorage.setItem(key, object);
+    }
+}
--- files/javascript/view_student.js
+++ files/javascript/view_student.js
@@ -1,21 +1,22 @@
 let namee = window.localStorage.getItem("Selected_Student");
-{"Name":"adham","Gender":"male","GPA":"3333","ID":"22222","level":"1","status":"active","date":"2222-02-22","department":"asdfa","email":"asdfaa@gmai.com","phone":"99999999999"}
+
+
+let Genders = ['Male', 'Female'];
+let statutes = ['Active', 'Inactive']
 
 document.getElementById('name').value = JSON.parse(namee).Name;
-document.getElementById("Gen").value = JSON.parse(namee).Gender;
+
+document.getElementById('Gen').value = JSON.parse(namee).Gender;
+
+
+
 document.getElementById("GPA").value = JSON.parse(namee).GPA;
-document.getElementById("ID").value = JSON.parse(namee).ID;
-document.getElementById("sta").value = JSON.parse(namee).status;
+document.getElementById("ID").value = parseFloat(JSON.parse(namee).date);
+document.getElementById('level').value = JSON.parse(namee).level;
+
+//document.getElementById("sta").value = JSON.parse(namee).status;
 document.getElementById("date").value = JSON.parse(namee).date;
 document.getElementById("dep").value = JSON.parse(namee).department;
 document.getElementById("email").value = JSON.parse(namee).email;
 document.getElementById("phone").value = JSON.parse(namee).phone;
 
-
-
-//This Script redirects to search after viewing student
-document.getElementById("edit_button").onclick = function () {
-     location.href = "search_student.html";
-};
-//This should clear the previous element from the memory.
-window.localStorage.removeItem(Selected_Student);
