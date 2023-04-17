02f8db03c93e9b4596f0bdac65851bc4050efdff
Error message: AMFAM <abdulrahmanmfam2003@gmail.com>
2023-04-17 23:12:11 +0200

    i think it works

--- files/javascript/addDeb.js
+++ files/javascript/addDeb.js
@@ -1,65 +1,58 @@
 let button = document.querySelector(`.butt.b1`);
-let arrow = document.querySelector(`.department #dep>div:first-child`);
-let deps = document.querySelector(`.department .parent`);
-let ds = document.querySelectorAll(`.parent>div`)
+let departmentSelect = document.getElementById("depSelect");
 console.log(deps)
-const obj = {
-    name: "a",
-    Gen: "c",
-    cp: "d",
-    ID: "cx",
-    level: "cv",
-    sta: "",
-    date: "vc",
-    dep: "c",
-    email: "c",
-    phone: "cv"
+
+
+
+function backPage() {
+    window.location.href = "search_student.html";
 }
+
 
 window.onload = function() {
-    let editStudent = window.localStorage.getItem("editStudent");
-    editStudent = JSON.parse(editStudent)
-    let inputs = document.querySelectorAll("input");
-    let i = 0;
-    for (let a in editStudent) {
-        inputs[i].value = editStudent[a];
-        i++;
-        if (i == 9) {
-            break;
-        }
-    }
-    let a = 0;
-    arrow.addEventListener("click", function() {
-        if (deps.classList.contains("active")) {
-            deps.classList.remove("active");
-        } else {
-            deps.classList.add("active");
-        }
-        if (arrow.classList.contains("active")) {
-            arrow.classList.remove("active")
-        } else {
-            arrow.classList.add("active")
-        }
-    });
-    ds.forEach((ele) => {
-        ele.addEventListener("click", (event) => {
-            ds.forEach((e) => { e.classList.remove("active") })
-            event.target.classList.add("active");
-        })
-    });
+    let editStudent = window.localStorage.getItem("Selected_Student");
+    editStudent = JSON.parse(editStudent);
+    console.log(editStudent);
+    //load student data and display
+    document.getElementById('name').value = editStudent['Name'];
+    document.getElementById('Gen').value = editStudent['Gender'];
+    document.getElementById('GPA').value = editStudent['GPA'];
+    document.getElementById('ID').value = editStudent['ID'];
+    document.getElementById('level').value = editStudent['level'];
+    document.getElementById('sta').value = editStudent['status'];
+    document.getElementById('date').value = editStudent['date'];
+    document.getElementById('dep').value = editStudent['department'];
+    document.getElementById('email').value = editStudent['email'];
+    document.getElementById('phone').value = editStudent['phone'];
+
+    
+    // arrow.addEventListener("click", function() {
+    //     if (deps.classList.contains("active")) {
+    //         deps.classList.remove("active");
+    //     } else {
+    //         deps.classList.add("active");
+    //     }
+    //     if (arrow.classList.contains("active")) {
+    //         arrow.classList.remove("active")
+    //     } else {
+    //         arrow.classList.add("active")
+    //     }
+    // });
+
+    // ds.forEach((ele) => {
+    //     ele.addEventListener("click", (event) => {
+    //         ds.forEach((e) => { e.classList.remove("active") })
+    //         event.target.classList.add("active");
+    //     })
+    // });
+
     button.onclick = () => {
-        let aaa = document.querySelector(`.parent>div.active`);
-        editStudent["Department"] = aaa.dataset.dep;
-        console.log(editStudent["Department"])
-        console.log(editStudent);
+        let studentName = editStudent.Name;
+        let selectedDep = document.getElementById("depSelect").value;
+
+        editStudent["department"] = selectedDep;
+
         editStudent = JSON.stringify(editStudent);
-        window.localStorage.setItem("editStudent", editStudent);
+        window.localStorage.setItem(studentName, editStudent);
     }
 }
-
-// window.onload = () => {
-//     let obj = { name: "hzaem", Gender: "male", cpa: "2", ID: 20210525, Level: 3, Status: "active", DateBirth: "9-29-2003", Email: "hazemelbehary@gmail.com", Phone: "01118013540", Department: "IS" };
-// obj = JSON.stringify(obj);
-// let a = window.localStorage.setItem("editStudent", obj);
-
-// }
