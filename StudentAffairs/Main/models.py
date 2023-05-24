44e08586a629fa3d16871bfd79d9a2d3981f12d8
Mansour <mansour24@protonmail.com>
2023-05-24 23:02:49 +0300

    Main model created and tested

--- StudentAffairs/Main/models.py
+++ StudentAffairs/Main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    id = models.CharField(max_length=8, primary_key=True)
+    name = models.CharField(max_length=64)
+    dateOfBirth = models.DateTimeField()
+    email = models.CharField(max_length=64)
+    GPA = models.FloatField()
+    level = models.IntegerField()
+    Status = models.BooleanField()
+    phone = models.CharField(max_length=12)
