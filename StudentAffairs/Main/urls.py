30a1dfbcd47fa1a0483f97b3d0126ffe0a0057ae
Error message: AMFAM <abdulrahmanmfam2003@gmail.com>
2023-05-25 19:52:29 +0300

    refactoring

--- StudentAffairs/Main/urls.py
+++ StudentAffairs/Main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .views import main
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home', views.home, name='home'),
-    path('edit-student', views.edit, name='edit'),
-    path('add-student', views.addStudent, name='add'),
-    path('save', views.formSubmission, name='save'),
+    path('', main.index, name='index'),
+    path('home', main.home, name='home'),
+    path('edit-student', main.edit, name='edit'),
+    path('add-student', main.addStudent, name='add'),
+    path('save', main.formSubmission, name='save'),
 ]
