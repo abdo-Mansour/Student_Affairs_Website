8876011be2cb404781aedbc0326673d2cfa975fa
Mansour <mansour24@protonmail.com>
2023-05-25 15:03:22 +0300

    small path change

--- StudentAffairs/Main/urls.py
+++ StudentAffairs/Main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('home', views.home, name='home'),
     path('edit-student', views.edit, name='edit'),
 ]
